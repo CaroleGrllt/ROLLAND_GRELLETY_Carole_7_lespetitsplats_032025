60a2b738e5ac1e94ad83997496ee31577196769b
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-05-10 17:46:40 +0200

    Correction boucles natives - stateFilter

--- assets/js/state/stateFilter.js
+++ assets/js/state/stateFilter.js
@@ -1,83 +1,109 @@
-export default function StateFilter() {
-    this.searchLength = 0;
-    this.search = [];
-    this.appliances = [];
-    this.ingredients = [];
-    this.ustensils = [];
-    this.tags = [];
+export default class StateFilter {
+    constructor() {
+        this.searchLength   = 0;
+        this.search         = [];
+        this.appliances     = [];
+        this.ingredients    = [];
+        this.ustensils      = [];
+        this.tags           = [];
+    }
+
+    // Remplace split(" ") et toLowerCase() par version manuelle
+    setSearch(value) {
+        this.search = [];
+        this.searchLength = value.length;
+
+        let word = "";
+        let index = 0;
+
+        for (let i = 0; i <= value.length; i++) {
+            const char = i < value.length ? value[i].toLowerCase() : " ";
+            if (char === " ") {
+                if (word.length > 0) {
+                    this.search[index++] = word;
+                    word = "";
+                }
+            } else {
+                word += char;
+            }
+        }
+    }
+
+    unsetSearch() {
+        this.search = [];
+        this.searchLength = 0;
+    }
+
+    setIngredients(value) {
+        const val = value.toLowerCase();
+        const len = this.ingredients.length;
+        this.ingredients[len] = val;
+    }
+
+    unsetIngredients(value) {
+        const newArr = [];
+        let index = 0;
+        for (let i = 0; i < this.ingredients.length; i++) {
+            if (this.ingredients[i] !== value) {
+                newArr[index++] = this.ingredients[i];
+            }
+        }
+        this.ingredients = newArr;
+    }
+
+    setAppliances(value) {
+        const val = value.toLowerCase();
+        const len = this.appliances.length;
+        this.appliances[len] = val;
+    }
+
+    unsetAppliances() {
+        this.appliances = [];
+    }
+
+    setUstensils(value) {
+        const val = value.toLowerCase();
+        const len = this.ustensils.length;
+        this.ustensils[len] = val;
+    }
+
+    unsetUstensils(value) {
+        const newArr = [];
+        let index = 0;
+        for (let i = 0; i < this.ustensils.length; i++) {
+            if (this.ustensils[i] !== value) {
+                newArr[index++] = this.ustensils[i];
+            }
+        }
+        this.ustensils = newArr;
+    }
+
+    setTags(value) {
+        const len = this.tags.length;
+        this.tags[len] = value;
+    }
+
+    getTags() {
+        return this.tags;
+    }
+
+    unsetTags(value) {
+        const newArr = [];
+        let index = 0;
+        for (let i = 0; i < this.tags.length; i++) {
+            if (this.tags[i] !== value) {
+                newArr[index++] = this.tags[i];
+            }
+        }
+        this.tags = newArr;
+    }
+
+    unsetAll() {
+        this.searchLength   = 0;
+        this.search         = [];
+        this.appliances     = [];
+        this.ingredients    = [];
+        this.ustensils      = [];
+        this.tags           = [];
+    }
 }
-
-StateFilter.prototype.setSearch = function(value) {
-    this.search = value.toLowerCase().split(' ');
-    this.searchLength = value.length;
-};
-
-StateFilter.prototype.unsetSearch = function() {
-    this.search = [];
-    this.searchLength = 0;
-};
-
-StateFilter.prototype.setIngredients = function(value) {
-    this.ingredients.push(value.toLowerCase());
-};
-
-StateFilter.prototype.unsetIngredients = function(value) {
-    const result = [];
-    for (let i = 0; i < this.ingredients.length; i++) {
-        if (this.ingredients[i] !== value) {
-            result.push(this.ingredients[i]);
-        }
-    }
-    this.ingredients = result;
-};
-
-StateFilter.prototype.setAppliances = function(value) {
-    this.appliances.push(value.toLowerCase());
-};
-
-StateFilter.prototype.unsetAppliances = function() {
-    this.appliances = [];
-};
-
-StateFilter.prototype.setUstensils = function(value) {
-    this.ustensils.push(value.toLowerCase());
-};
-
-StateFilter.prototype.unsetUstensils = function(value) {
-    const result = [];
-    for (let i = 0; i < this.ustensils.length; i++) {
-        if (this.ustensils[i] !== value) {
-            result.push(this.ustensils[i]);
-        }
-    }
-    this.ustensils = result;
-};
-
-StateFilter.prototype.setTags = function(value) {
-    this.tags.push(value);
-};
-
-StateFilter.prototype.getTags = function() {
-    return this.tags;
-};
-
-StateFilter.prototype.unsetTags = function(value) {
-    const result = [];
-    for (let i = 0; i < this.tags.length; i++) {
-        if (this.tags[i] !== value) {
-            result.push(this.tags[i]);
-        }
-    }
-    this.tags = result;
-};
-
-StateFilter.prototype.unsetAll = function() {
-    this.searchLength = 0;
-    this.search = [];
-    this.appliances = [];
-    this.ingredients = [];
-    this.ustensils = [];
-    this.tags = [];
-};
-
-

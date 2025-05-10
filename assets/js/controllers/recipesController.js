72302d497bdc0bcfcd11bea72f4a7b399f3e6f90
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-05-10 17:43:19 +0200

    Correction boucles natives - recipesController

--- assets/js/controllers/recipesController.js
+++ assets/js/controllers/recipesController.js
@@ -1,193 +1,250 @@
 import dataRecipes from "../models/recipesModel.js"
 
 export async function getRecipes(state) {
-    const data = dataRecipes();
-    let recipes = await data.getAllRecipes();
+    const data = dataRecipes()
+    let recipes = await data.getAllRecipes()
 
-    // SEARCH FIELD
     if (state.searchLength >= 3) {
-        const filteredRecipes = [];
+        const filtered = []
+        let fIndex = 0
 
         for (let i = 0; i < recipes.length; i++) {
             const recipe = recipes[i];
 
-            const nameWords = recipe.name.toLowerCase().split(' ');
-            const descWords = recipe.description.toLowerCase().split(' ');
-
-            const ingredientWords = [];
-            for (let j = 0; j < recipe.ingredients.length; j++) {
-                ingredientWords.push(recipe.ingredients[j].ingredient.toLowerCase());
+            function manualSplit(str) {
+                const result = [];
+                let rIndex = 0;
+                let current = "";
+                for (let j = 0; j <= str.length; j++) {
+                    if (j === str.length || str[j] === " ") {
+                        if (current.length > 0) {
+                            result[rIndex++] = current;
+                        }
+                        current = "";
+                    } else {
+                        current += str[j];
+                    }
+                }
+                return result;
             }
 
-            const allWords = nameWords.concat(descWords).concat(ingredientWords);
+            const name = manualSplit(recipe.name.toLowerCase());
+            const description = manualSplit(recipe.description.toLowerCase());
 
-            let matchesAll = true;
+            const ingredients = [];
+            let ingIndex = 0;
+            for (let j = 0; j < recipe.ingredients.length; j++) {
+                ingredients[ingIndex++] = recipe.ingredients[j].ingredient.toLowerCase();
+            }
+
+            const dataWords = [];
+            let dIndex = 0;
+            for (let j = 0; j < name.length; j++) dataWords[dIndex++] = name[j];
+            for (let j = 0; j < description.length; j++) dataWords[dIndex++] = description[j];
+            for (let j = 0; j < ingredients.length; j++) dataWords[dIndex++] = ingredients[j];
+
+            let allMatch = true;
             for (let k = 0; k < state.search.length; k++) {
                 const word = state.search[k].toLowerCase();
                 let found = false;
-                for (let l = 0; l < allWords.length; l++) {
-                    if (allWords[l].includes(word)) {
+                for (let l = 0; l < dataWords.length; l++) {
+                    if (dataWords[l].indexOf(word) !== -1) {
                         found = true;
                         break;
                     }
                 }
                 if (!found) {
-                    matchesAll = false;
+                    allMatch = false;
                     break;
                 }
             }
 
-            if (matchesAll) {
-                filteredRecipes.push(recipe);
+            if (allMatch) {
+                filtered[fIndex++] = recipe;
             }
         }
 
-        recipes = filteredRecipes;
+        recipes = filtered;
     }
 
-    // INGREDIENTS
     if (state.ingredients.length > 0) {
-        const filteredRecipes = [];
+        const filtered = [];
+        let fIndex = 0;
 
         for (let i = 0; i < recipes.length; i++) {
             const recipe = recipes[i];
-
             const ingredients = [];
+            let ingIndex = 0;
+
             for (let j = 0; j < recipe.ingredients.length; j++) {
-                ingredients.push(recipe.ingredients[j].ingredient.toLowerCase());
+                ingredients[ingIndex++] = recipe.ingredients[j].ingredient.toLowerCase();
             }
 
-            let matchesAll = true;
-            for (let k = 0; k < state.ingredients.length; k++) {
-                const wantedIng = state.ingredients[k].toLowerCase();
+            let allFound = true;
+            for (let j = 0; j < state.ingredients.length; j++) {
+                const searchIng = state.ingredients[j].toLowerCase();
                 let found = false;
-
-                for (let l = 0; l < ingredients.length; l++) {
-                    if (ingredients[l].includes(wantedIng)) {
+                for (let k = 0; k < ingredients.length; k++) {
+                    if (ingredients[k].indexOf(searchIng) !== -1) {
                         found = true;
                         break;
                     }
                 }
-
                 if (!found) {
-                    matchesAll = false;
+                    allFound = false;
                     break;
                 }
             }
 
-            if (matchesAll) {
-                filteredRecipes.push(recipe);
+            if (allFound) {
+                filtered[fIndex++] = recipe;
             }
         }
 
-        recipes = filteredRecipes;
+        recipes = filtered;
     }
 
-    // APPLIANCES
     if (state.appliances.length > 0) {
-        const filteredRecipes = [];
+        const filtered = [];
+        let fIndex = 0;
 
         for (let i = 0; i < recipes.length; i++) {
-            const recipe = recipes[i];
-            const appliance = recipe.appliance.toLowerCase();
+            const appliance = recipes[i].appliance.toLowerCase();
 
-            let found = false;
             for (let j = 0; j < state.appliances.length; j++) {
                 if (state.appliances[j].toLowerCase() === appliance) {
-                    found = true;
+                    filtered[fIndex++] = recipes[i];
                     break;
                 }
             }
-
-            if (found) {
-                filteredRecipes.push(recipe);
-            }
         }
 
-        recipes = filteredRecipes;
+        recipes = filtered;
     }
 
-    // USTENSILS
     if (state.ustensils.length > 0) {
-        const filteredRecipes = [];
+        const filtered = [];
+        let fIndex = 0;
 
         for (let i = 0; i < recipes.length; i++) {
-            const recipe = recipes[i];
-
             const ustensils = [];
-            for (let j = 0; j < recipe.ustensils.length; j++) {
-                ustensils.push(recipe.ustensils[j].toLowerCase());
+            let uIndex = 0;
+
+            for (let j = 0; j < recipes[i].ustensils.length; j++) {
+                ustensils[uIndex++] = recipes[i].ustensils[j].toLowerCase();
             }
 
-            let matchesAll = true;
-            for (let k = 0; k < state.ustensils.length; k++) {
-                const wantedUst = state.ustensils[k].toLowerCase();
+            let allFound = true;
+            for (let j = 0; j < state.ustensils.length; j++) {
+                const searchUst = state.ustensils[j].toLowerCase();
                 let found = false;
 
-                for (let l = 0; l < ustensils.length; l++) {
-                    if (ustensils[l].includes(wantedUst)) {
+                for (let k = 0; k < ustensils.length; k++) {
+                    if (ustensils[k].indexOf(searchUst) !== -1) {
                         found = true;
                         break;
                     }
                 }
 
                 if (!found) {
-                    matchesAll = false;
+                    allFound = false;
                     break;
                 }
             }
 
-            if (matchesAll) {
-                filteredRecipes.push(recipe);
+            if (allFound) {
+                filtered[fIndex++] = recipes[i];
             }
         }
 
-        recipes = filteredRecipes;
+        recipes = filtered;
     }
 
     return recipes;
 }
 
 export function getIngredientsList(recipes) {
-    const ingredientsSet = new Set();
+    const ingredientsSet = {};
 
     for (let i = 0; i < recipes.length; i++) {
-        const recipe = recipes[i];
-
-        for (let j = 0; j < recipe.ingredients.length; j++) {
-            ingredientsSet.add(recipe.ingredients[j].ingredient.toLowerCase());
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+            const ing = recipes[i].ingredients[j].ingredient.toLowerCase();
+            ingredientsSet[ing] = true;
         }
     }
 
-    const list = Array.from(ingredientsSet);
-    list.sort();
-    return list;
+    const ingredientsArray = [];
+    let index = 0;
+    for (let key in ingredientsSet) {
+        ingredientsArray[index++] = key;
+    }
+
+    // manual sort
+    for (let i = 0; i < ingredientsArray.length - 1; i++) {
+        for (let j = i + 1; j < ingredientsArray.length; j++) {
+            if (ingredientsArray[i] > ingredientsArray[j]) {
+                const tmp = ingredientsArray[i];
+                ingredientsArray[i] = ingredientsArray[j];
+                ingredientsArray[j] = tmp;
+            }
+        }
+    }
+
+    return ingredientsArray;
 }
 
 export function getAppliancesList(recipes) {
-    const appliancesSet = new Set();
+    const appliancesSet = {};
 
     for (let i = 0; i < recipes.length; i++) {
-        appliancesSet.add(recipes[i].appliance.toLowerCase());
+        const appliance = recipes[i].appliance.toLowerCase();
+        appliancesSet[appliance] = true;
     }
 
-    const list = Array.from(appliancesSet);
-    list.sort();
-    return list;
-}
+    const appliancesArray = [];
+    let index = 0;
+    for (let key in appliancesSet) {
+        appliancesArray[index++] = key;
+    }
 
-export function getUstensilsList(recipes) {
-    const ustensilsSet = new Set();
-
-    for (let i = 0; i < recipes.length; i++) {
-        const recipe = recipes[i];
-
-        for (let j = 0; j < recipe.ustensils.length; j++) {
-            ustensilsSet.add(recipe.ustensils[j].toLowerCase());
+    for (let i = 0; i < appliancesArray.length - 1; i++) {
+        for (let j = i + 1; j < appliancesArray.length; j++) {
+            if (appliancesArray[i] > appliancesArray[j]) {
+                const tmp = appliancesArray[i];
+                appliancesArray[i] = appliancesArray[j];
+                appliancesArray[j] = tmp;
+            }
         }
     }
 
-    const list = Array.from(ustensilsSet);
-    list.sort();
-    return list;
+    return appliancesArray;
+}
+
+export function getUstensilsList(recipes) {
+    const ustensilsSet = {};
+
+    for (let i = 0; i < recipes.length; i++) {
+        for (let j = 0; j < recipes[i].ustensils.length; j++) {
+            const ust = recipes[i].ustensils[j].toLowerCase();
+            ustensilsSet[ust] = true;
+        }
+    }
+
+    const ustensilsArray = [];
+    let index = 0;
+    for (let key in ustensilsSet) {
+        ustensilsArray[index++] = key;
+    }
+
+    for (let i = 0; i < ustensilsArray.length - 1; i++) {
+        for (let j = i + 1; j < ustensilsArray.length; j++) {
+            if (ustensilsArray[i] > ustensilsArray[j]) {
+                const tmp = ustensilsArray[i];
+                ustensilsArray[i] = ustensilsArray[j];
+                ustensilsArray[j] = tmp;
+            }
+        }
+    }
+
+    return ustensilsArray;
 }

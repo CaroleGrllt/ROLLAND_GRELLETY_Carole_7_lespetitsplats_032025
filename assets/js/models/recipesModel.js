f1e21ceb63a12d868fac9b3e605f4721476ef43d
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-09-26 17:16:01 +0200

    Change model alternative loop

--- assets/js/models/recipesModel.js
+++ assets/js/models/recipesModel.js
@@ -7,15 +7,16 @@
 export default function dataRecipes() {
 
     async function getData() {
-        try {
-            const url = new URL('../../assets/data/recipes.json', import.meta.url);
-            const res = await fetch(url);
-            return await res.json();
-        } catch (err) {
-            console.log("Une erreur s'est produite dans la récupération des données json :", err);
-            return { recipes: [] };
-        }
-    }
+    try {
+      const url = new URL('../../data/recipes.json', import.meta.url);
+      console.log(url.href);
+      const res = await fetch(url);
+      if (!res.ok) throw new Error(`HTTP ${res.status} sur ${res.url}`);
+      return await res.json();
+    } catch (err) {
+      console.error("Récupération JSON : ", err);
+      return { recipes: [] }; // évite le TypeError après coup
+    }    }
 
     async function getAllRecipes() {
         let data = await getData()

f76a29e1d8092b81c041d96fa35a4123233534b5
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-09-26 17:09:30 +0200

    Pb fichier json

--- assets/js/models/recipesModel.js
+++ assets/js/models/recipesModel.js
@@ -7,15 +7,15 @@
 export default function dataRecipes() {
 
     async function getData() {
-        try {
-            const url = new URL('../../assets/data/recipes.json', import.meta.url);
-            const res = await fetch(url);
-            return await res.json();
-        } catch (err) {
-            console.log("Une erreur s'est produite dans la récupération des données json :", err);
-            return { recipes: [] };
-        }
-    }
+    try {
+      const url = new URL('../../assets/data/recipes.json', import.meta.url);
+      const res = await fetch(url);
+      if (!res.ok) throw new Error(`HTTP ${res.status} sur ${res.url}`);
+      return await res.json();
+    } catch (err) {
+      console.error("Récupération JSON : ", err);
+      return { recipes: [] }; // évite le TypeError après coup
+    }    }
 
     async function getAllRecipes() {
         let data = await getData()
